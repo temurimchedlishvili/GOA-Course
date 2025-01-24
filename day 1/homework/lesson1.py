d1be6a2b495c9e619c293ac91b52c204e1d38a04
temurimchedlishvili <temurimchedlishvili239@gmail.com>
2025-01-24 15:16:11 +0400

    first commit

---
+++ day 1/homework/lesson1.py
@@ -0,0 +1,35 @@
+name="temo"
+#name არის ცვლადი
+# = ცვლადისთვის მიმნიჭებელი სიმბოლო
+# "ცვლადისთვის მნიშვნელობა"
+
+surname="mchedlishvili"
+
+#print(name)
+#პრინტ პუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name="temo" #ეს არის str (string ) ტიპის ცვლადი
+age= 19 #ეს არის Int (integer ) მთელი რიცხვი
+height= 185.0 #ეს არის (float) ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი 
+
+knows_proggraming=True  #true ან false
+is_ugly = False  #snakecase( უბრალოდ წერის სტილი სტანდარტულად )
+
+is_Ugly=False  #ჯავასქრიპტული  camelcase
+
+
+#print(name +" "+ surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_proggraming))
+
+print(name + " " + str(age))
+
+print(name + surname + str(age) + (height) + (knows_proggraming)) 
