4c23aafe0e85a186f392fa657e275198435e8bcd
temurimchedlishvili <temurimchedlishvili239@gmail.com>
2025-03-08 19:22:06 +0400

    6th commit

--- day 8/homework/homework4.py
+++ day 8/homework/homework4.py
@@ -1,9 +1,7 @@
-while True:
-    num = int(input("შეიყვანეთ რიცხვი (1-50): "))
-    if 1 <= num <= 50:
-        break
-    print("გთხოვთ, შეიყვანოთ 1-დან 50-მდე რიცხვი.")
-
-print(f"{num}-ის ჯერადები 100-ის ჩათვლით:")
-for i in range(num, 101, num):
-    print(i)
+num = int(input("chaweret ricxvi 50-is chatvlit: "))
+while num > 50 or num <1:
+    print("tqveni ricxvi agemateba 50-s sheiyvanet ricxvi axlidan: ")
+    num = int(input("chaweret ricxvi 50-is chatvlit: "))
+for i in range(num,101):
+    if i % num == 0:
+        print(i)    
