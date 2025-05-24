67351f3ec7f5ba0e642e8f17f627c466514f45bc
temurimchedlishvili <temurimchedlishvili239@gmail.com>
2025-05-24 15:38:54 +0400

    commit

---
+++ day 29/homework/homewok5.py
@@ -0,0 +1,6 @@
+#შეისწავლეთ chr და ord Python-ში, რესურსები:,
+
+# chr(): https://www.w3schools.com/python/ref_func_chr.asp
+# ord(): https://www.w3schools.com/python/ref_func_ord.asp
+
+# შევისწავლე ორივე და ვუყურე კიდეც ვიდეოებს
